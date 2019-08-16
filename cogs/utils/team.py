235a309c35faf16c38a81810219deb1466d3b565
Brandon <git@bsquid.io>
2019-08-16 09:34:54 -0700

    Indentation cause derp

--- cogs/utils/team.py
+++ cogs/utils/team.py
@@ -2,15 +2,15 @@ import re
 
 
 def get_team(message) -> str:
-        try:
-                regex = r"#(save|shave)"
-                matches = re.finditer(regex, message, re.MULTILINE | re.IGNORECASE)
+    try:
+        regex = r"#(save|shave)"
+        matches = re.finditer(regex, message, re.MULTILINE | re.IGNORECASE)
 
-                for matchNum, match in enumerate(matches, start=1):
-                        if match.group():
-                        return match.group()
+        for matchNum, match in enumerate(matches, start=1):
+            if match.group():
+                return match.group()
 
-        except:
-                pass
-        finally:
-                return None
+    except:
+        pass
+    finally:
+        return None
