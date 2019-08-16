dc265f2ad08723caaa40cb06277f1b4b9f566328
Andrew <aniemann1@gmail.com>
2019-08-16 13:50:38 -0400

    Add full docker commands

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#1/bin/sh
+#pull latest image and restart bot
+
+docker pull bsquidwrd/beardbot:latest
+docker-compose down --remove-orphans 
+sleep 5
 docker-compose up --build -d --remove-orphans
 docker exec beardbot_bot_1 python manage.py migrate
 docker-compose logs -f
