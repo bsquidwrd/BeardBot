b5a84dca123113a729fb3b4fd9ecfd3d31d2756d
Brandon <git@bsquid.io>
2019-08-16 09:57:52 -0700

    CHange team selecting

--- cogs/utils/team.py
+++ cogs/utils/team.py
@@ -5,12 +5,12 @@ def get_team(message) -> str:
     try:
         regex = r"#(save|shave)"
         matches = re.finditer(regex, message, re.MULTILINE | re.IGNORECASE)
-
+        print(matches)
         for matchNum, match in enumerate(matches, start=1):
             if match.group():
                 return match.group()
 
     except:
         pass
-    finally:
-        return None
+
+    return None
