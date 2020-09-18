6f7c7f3184f43a329501f4eb2185029bb747c938
Brandon <git@bsquid.io>
2020-09-18 08:52:45 -0700

    Change perms for is_mod to include owner check too

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -4,4 +4,4 @@ def is_owner(ctx):
     return ctx.message.author.id == int(os.environ['OWNER_ID'])
 
 def is_mod(ctx):
-    return ctx.message.author.is_mod == 1
+    return ctx.message.author.is_mod == 1 or is_owner(ctx)
